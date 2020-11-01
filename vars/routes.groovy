3f00290d20875566d10b56a60b815134874a2879
Guillermo Sarabia <guillermo.sarabia@gmail.com>
2020-11-01 11:29:43 +0100

    Change to static

--- vars/routes.groovy
+++ vars/routes.groovy
@@ -1,3 +1,7 @@
-def kpsrThirdPartiesECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-thirdparties"
-def kpsrCoreECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-core"
-def kpsrRoboticsECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-robotics"
+#!/usr/bin/env groovy
+
+class githubECRRoutes {
+  def kpsrThirdPartiesECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-thirdparties"
+  def kpsrCoreECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-core"
+  def kpsrRoboticsECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-robotics"
+}
