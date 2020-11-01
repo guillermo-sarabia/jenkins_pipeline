391e76b74e154fdbd9456debecd86b3ef51e3ef6
Guillermo Sarabia <guillermo.sarabia@gmail.com>
2020-11-01 11:12:54 +0100

    Add routes test.

---
+++ vars/routes.groovy
@@ -0,0 +1,3 @@
+def kpsrThirdPartiesECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-thirdparties"
+def kpsrCoreECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-core"
+def kpsrRoboticsECR="337955887028.dkr.ecr.us-east-2.amazonaws.com/kpsr-docker-registry/github/kpsr-robotics"
